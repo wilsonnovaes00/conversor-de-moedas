5d231192344c36a018db35c74c78923f61e0aaad
Raquel <raquel.costa@ttwgroup.com>
2017-08-17 17:08:09 -0300

    Create module conversor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ConversorComponent } from './conversor/conversor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/conversor/conversor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-conversor',
+  templateUrl: './conversor.component.html',
+  styleUrls: ['./conversor.component.css']
+})
+export class ConversorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
