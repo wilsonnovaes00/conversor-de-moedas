853d9b43c1e85c8f34cc5ad9493863c1a6d9430a
Raquel <raquel.costa@ttwgroup.com>
2017-08-18 11:21:43 -0300

    map and catch add in file polyfills

--- src/app/conversor/services/conversor.service.ts
+++ src/app/conversor/services/conversor.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
 import { SRC } from '../../app.api';
+import { Observable } from 'rxjs/Observable';
+import { ConversaoResponse, Conversao } from '../models';
 
 @Injectable()
 export class ConversorService {
 
   constructor(private http: Http) { }
 
+  converter(conversao: Conversao): Observable<ConversaoResponse> {
+    let params = `?base=${conversao.moedaFrom}&symbols=${conversao.moedaTo}`;
+  }
 }
